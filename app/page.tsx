ed8e0ed2418f230f1aa49bca8ed71cd608aa5e47
Laura Ku <lauraku888@gmail.com>
2024-02-23 20:59:08 +0900

    lint

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
 
       <div className="mb-64">
         <button className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20">
-          Let's Begin
+          Let&apos;s Begin
         </button>
       </div>
     </main>
