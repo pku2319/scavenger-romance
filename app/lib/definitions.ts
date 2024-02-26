77724b58f36a74c4002fcc717039e8f5434b4a1d
Laura Ku <lauraku888@gmail.com>
2024-02-26 15:33:46 +0900

    Add a board field
    - Also test data

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -2,4 +2,5 @@ export type Traveler = {
   id: string; // Will be created on the database
   name: string;
   game: string;
+  board: Array<number>;
 };
--- scripts/seed.js
+++ scripts/seed.js
@@ -1,5 +1,14 @@
 const { db } = require('@vercel/postgres');
 
+const travelers = [
+  {
+    id: 'e646456e-0906-480a-97e0-b13c674ec1c9',
+    name: 'Avery',
+    game: 'test',
+    board: JSON.stringify([0, 0, 1, 0, 0, 0, 0, 0, 0])
+  }
+]
+
 async function seedTravelers(client) {
   try {
     await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
@@ -9,35 +18,73 @@ async function seedTravelers(client) {
         id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
         name VARCHAR(255) NOT NULL,
         game TEXT NOT NULL,
+        board JSONB NOT NULL,
         CONSTRAINT game_user UNIQUE (game, name)
       );
     `;
 
     console.log(`Created "travelers" table`);
 
-    // // Insert data into the "travelers" table
-    // const insertedTravelers = await Promise.all(
-    //   travelers.map(async (traveler) => {
-    //     const hashedPassword = await bcrypt.hash(traveler.password, 10);
-    //     return client.sql`
-    //     INSERT INTO travelers (id, name, password)
-    //     VALUES (${traveler.id}, ${traveler.name}, ${hashedPassword})
-    //     ON CONFLICT (id) DO NOTHING;
-    //   `;
-    //   }),
-    // );
+    // Insert data into the "travelers" table
+    const insertedTravelers = await Promise.all(
+      travelers.map(async (traveler) => {
+        return client.sql`
+        INSERT INTO travelers (id, name, game, board)
+        VALUES (${traveler.id}, ${traveler.name}, ${traveler.game}, ${traveler.board})
+        ON CONFLICT (id) DO NOTHING;
+      `;
+      }),
+    );
 
-    // console.log(`Seeded ${insertedTravelers.length} travelers`);
+    console.log(`Seeded ${insertedTravelers.length} travelers`);
 
     return {
       createTable,
-      // travelers: insertedTravelers,
+      travelers: insertedTravelers,
     };
   } catch (error) {
     console.error('Error seeding travelers:', error);
     throw error;
   }
 }
+
+// async function seedBoards(client) {
+//   try {
+//     await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+//     // Create the "boards" table if it doesn't exist
+//     const createTable = await client.sql`
+//       CREATE TABLE IF NOT EXISTS boards (
+//         id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+//         travelerId UUID NOT NULL,
+//         values JSONB NOT NULL,
+//         CONSTRAINT traveler_board UNIQUE (travelerId)
+//       );
+//     `;
+
+//     console.log(`Created "boards" table`);
+
+//     // Insert data into the "boards" table
+//     const insertedBoards = await Promise.all(
+//       boards.map(async (board) => {
+//         return client.sql`
+//         INSERT INTO boards (id, travelerId, values)
+//         VALUES (${board.id}, ${board.travelerId}, ${board.values})
+//         ON CONFLICT (id) DO NOTHING;
+//       `;
+//       }),
+//     );
+
+//     console.log(`Seeded ${insertedBoards.length} boards`);
+
+//     return {
+//       createTable,
+//       boards: insertedBoards,
+//     };
+//   } catch (error) {
+//     console.error('Error seeding boards:', error);
+//     throw error;
+//   }
+// }
 
 async function main() {
   const client = await db.connect();
