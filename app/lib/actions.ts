f1f14f39b22bf25efd616e637be9b767768465bc
Laura Ku <lauraku888@gmail.com>
2024-03-19 22:35:19 +0900

    Still use cookies for client side fetching

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,8 +4,10 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { drizzle } from 'drizzle-orm/vercel-postgres';
 import { revalidatePath } from 'next/cache';
+import { cookies } from 'next/headers';
 
 import { travelers, pieces } from '@/schema';
+import { Traveler } from './definitions';
 
 const FormSchema = z.object({
   id: z.string(),
@@ -19,20 +21,25 @@ const FormSchema = z.object({
 
 const db = drizzle(sql);
 
+function isNew(model: any) {
+  return model.createdAt === model.updatedAt;
+}
+
 export async function createTraveler(data: { name: string; game: string; email: string }) {
-  let result: { id: string; name: string; game: string; email: string; createdAt: Date; updatedAt: Date; }[];
+  let result: Traveler[];
 
   const { name, game, email } = data
   const createdAt = new Date();
   const updatedAt = new Date();
 
-  const pieceIds = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
   try {
     result = await db
       .insert(travelers)
       .values({ name, email, game, createdAt, updatedAt })
-      .onConflictDoNothing()
+      .onConflictDoUpdate({
+        target: [travelers.email, travelers.game],
+        set: { updatedAt },
+      })
       .returning();
   } catch (e) {
     console.log(e)
@@ -42,10 +49,21 @@ export async function createTraveler(data: { name: string; game: string; email: 
     };
   }
 
+  if (isNew(result[0])) {
+    await createPieces(result[0].id, createdAt, updatedAt);
+  }
+
+  cookies().set('traveler', result[0].id);
+  revalidatePath('/');
+}
+
+async function createPieces(travelerId: string, createdAt: Date, updatedAt: Date) {
+  const pieceIds = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
   try {
     pieceIds.map(async (pieceId) => {
       await db.insert(pieces).values({
-        travelerId: result[0].id,
+        travelerId,
         status: 0,
         pieceId,
         answer: null,
@@ -61,8 +79,6 @@ export async function createTraveler(data: { name: string; game: string; email: 
       message: 'Database Error: Failed to Create Piece.',
     };
   }
-
-  revalidatePath('/');
 }
 
 // TODO: This needs to be updated to the ORM way of updating
