e224cdcc74c610ec5b1a09dceb12dfbb794f2c45
Laura Ku <lauraku888@gmail.com>
2024-03-20 10:43:04 +0900

    Make the signin/signup flow make more sense

--- app/ui/form/LoginForm.tsx
+++ app/ui/form/LoginForm.tsx
@@ -1,35 +1,10 @@
 import SignInButton from "../sign-in/components/SignInButton";
-import Input from "./components/Input";
-import Label from "./components/Label";
 
 export default function LoginForm() {
-  const initialState = { message: null, errors: {} };
-
   return (
     <div
-      className="flex flex-col items-center justify-between p-16 mt-20"
-    >
+      className="flex flex-col items-center justify-between pt-20 pb-10 mt-10">
       <SignInButton />
-      {/* <div className="flex flex-col items-center">
-        <Label htmlFor='email' text="Email:" />
-        <Input name="email" type="text" placeholder="" />
-        <Label htmlFor='password' text="Password:" />
-        <input
-          className="h-4 p-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2 rounded"
-          id="password"
-          type="password"
-          name="password"
-          placeholder="Enter password"
-          required
-          minLength={6}
-        />
-      </div> */}
-
-      {/* <div>
-        <button type="submit" className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20 mt-5">
-          Login
-        </button>
-      </div> */}
     </div>
   );
 }
--- app/ui/sign-in/components/SignInButton.tsx
+++ app/ui/sign-in/components/SignInButton.tsx
@@ -1,22 +1,10 @@
 "use client";
 
 import { useState } from "react";
-import { signIn, signOut, useSession } from "next-auth/react"
+import { signIn } from "next-auth/react"
 
 export default function SignInButton() {
   const [loading, setLoading] = useState(false)
-  const { data: session } = useSession()
-
-  if (session && session.user) {
-    return (
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={() => signOut()}
-      >
-        Sign Out
-      </button>
-    )
-  }
 
   if (loading) {
     return (
@@ -33,7 +21,7 @@ export default function SignInButton() {
       onClick={() => { setLoading(true); signIn() }}
       className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
     >
-      Sign In
+      Let&apos;s Begin
     </button>
   )
 } 
